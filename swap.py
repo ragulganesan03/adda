9be25929e281d49a021b78c06b6a4e098e1ef69a
ragulganesan03 <ragulganesan03@gmail.com>
2024-04-08 15:55:52 +0530

    first commit

---
+++ swap.py
@@ -0,0 +1,11 @@
+def swap_numbers(a, b):
+    print("Before swapping: a =", a, "and b =", b)
+    temp = a
+    a = b
+    b = temp
+    
+    print("After swapping: a =", a, "and b =", b)
+num1 = 10
+num2 = 20
+swap_numbers(num1, num2)
+print("21MIC0174 \n Ragul G")
